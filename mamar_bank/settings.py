9c3d97eef3e4c9349f33849b1c291701b1a09604
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-06-19 11:20:29 +0600

    mission accomplished 🔥🚀🔥

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-test-g553.onrender.com', 'https://*.127.0.0.1']
 
 # Application definition
 
